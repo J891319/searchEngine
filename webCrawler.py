f2281d5695727d8fdadd3a99343b04b84dc22182
James Deal <jamesdeal89@protonmail.com>
2018-08-02 08:31:58 +0100

    updated crawler into a mapping

--- webCrawler.py
+++ webCrawler.py
@@ -1,6 +1,6 @@
-index = []
-page = ''
-startLink = page.find("<a href=")
-url = page[startLink + 9 : page.find('">', startLink)]
-index.append(url)
-print(index)
+def getNextTarget(page):
+	startLink = page.find("<a href=")
+	startQuote = page.find('"', startLink)
+	endQuote = page.find('"', startQuote + 1)
+	url = page[startQuote + 1 : endQuote]
+	return url, endQuote
