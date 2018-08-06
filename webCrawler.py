8ea73540cef6ae29ca5220141e9cb0cbdbe2dd9d
James Deal <jamesdeal89@protonmail.com>
2018-08-06 09:35:11 +0100

    crawler gets all links on a page

--- webCrawler.py
+++ webCrawler.py
@@ -1,6 +1,25 @@
+def get_url(url):
+	import urllib.request 
+	import urllib
+	source = urllib.request.urlopen("http://xkcd.com").read().decode('utf-8')
+	return source
+
 def getNextTarget(page):
 	startLink = page.find("<a href=")
+	if startLink == -1:
+		return None, 0
 	startQuote = page.find('"', startLink)
 	endQuote = page.find('"', startQuote + 1)
 	url = page[startQuote + 1 : endQuote]
 	return url, endQuote
+
+def findAllLinks(page):
+	while True:
+		url, endpos = getNextTarget(page)
+		if url:
+			print(url)
+			page = page[endpos:]
+		else:
+			break
+
+findAllLinks(get_url("https://xkcd.com/"))
